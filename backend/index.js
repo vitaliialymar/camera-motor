dc8dc25ad309cbcba82078171924cda29e0c1bb8
Vitaliia Lymar <vitaliialymar@gmail.com>
2023-10-17 15:41:24 +0400

    fix some styles

--- backend/index.js
+++ backend/index.js
@@ -88,7 +88,7 @@ app.get('/api/v1/auth', authenticate, (req, res) => {
   res.send({ valid: true })
 })
 
-const PORT = 3001
+const PORT = "190.168.1.3:8081"
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
